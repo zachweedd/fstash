859127f083c06c8bbb652f66e50bfb0a1db17633
Zach Weed <zweed@covermymeds.com>
2018-01-17 18:20:12 -0500

    Initial commit

---
+++ search_stash.sh
@@ -0,0 +1,16 @@
+function search_stash () {
+  STASH_COUNT=`git stash list | tail -n1 | awk '{print substr($1,8,2)}'`
+
+  if [ "$STASH_COUNT" -ge "0" ]; then
+    for i in `seq 0 $STASH_COUNT`
+    do
+      FOUND_IN_STASH=`git stash show stash@{${i}} | grep -c $1`
+      if [ "$FOUND_IN_STASH" -ge 1 ]; then
+        echo "Your file was found in stash number: $i"
+        break
+      fi
+    done
+    echo "We were unable to find the specified file"
+  fi
+
+}
